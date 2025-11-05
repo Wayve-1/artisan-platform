9d6209b7a00f442669f78fd8f312563a959c1ee5
Wayve-1 <ju.wayve@gmail.com>
2025-11-05 15:25:13 +0100

    Fix console.log database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
