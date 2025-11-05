b2ee586fad87cbb7b9e8d56eb3bc799dbc72fcc6
Wayve-1 <ju.wayve@gmail.com>
2025-11-05 15:31:02 +0100

    Ajout consol.log database

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,13 @@ require("dotenv").config();
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
+console.log("ENV VARS:", {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD ? "****" : undefined,
+  DB_NAME: process.env.DB_NAME,
+});
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
