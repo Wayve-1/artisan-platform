3e124f94c49c23a5c76d12b73f36dea1dfeeddf3
Wayve-1 <ju.wayve@gmail.com>
2025-11-18 02:39:58 +0100

    adaptation mysql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 
 (async () => {
   try {
