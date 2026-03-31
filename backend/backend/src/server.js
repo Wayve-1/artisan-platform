f8ebf4b5ce9cb9aa4299e524a74eac59662153f1
Wayve-1 <ju.wayve@gmail.com>
2026-03-31 14:42:44 +0200

    Fix: suppression du backend dupliqué + ajout du listen()

--- backend/backend/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("../app.js");
-
-console.log("Render DB_HOST:", process.env.DB_HOST);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
-});
