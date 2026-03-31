b920096caeed121779767e1ec36c3fb987396ee6
Wayve-1 <ju.wayve@gmail.com>
2026-03-31 13:51:49 +0200

    Fix chemins absolus + mise à jour app.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require("sequelize");
-console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 
 (async () => {
   try {
